448962431060be1bb9a239ffb355a3d77f1c2612
Matías Barrios <matias.barriosn@gmail.com>
2020-04-27 21:34:04 -0400

    humans!

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+enum Gender {
+    Male,
+    Female,
+}
+
+struct Human {
+    name: String,
+    age: u32,
+    gender: Gender,
+}
+
+fn main() {
+    let matias = Human {
+        name: String::from("Matias"),
+        age: 1231,
+        gender: Gender::Female,
+    };
+    let rosalis = Human {
+        name: String::from("Rosalis"),
+        age: 11231,
+        gender: Gender::Male,
+    };
+    println!(
+        "{} has {} years old, gender:{:?}",
+        matias.name, matias.age, matias.gender
+    );
+    println!(
+        "{} has {} years old, gender:{:?}",
+        rosalis.name, rosalis.age, rosalis.gender
+    );
+}
